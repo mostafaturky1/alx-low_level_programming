8c9f21839cf2d2da3432f9ed252674c052eeed63
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-08-10 14:57:33 -0700

    “commit”

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(void){
-    puts("\"Programming is like building a multilingual puzzle")
-    return (0);
+/**
+ *main - entry point
+ * 
+ *Description: a c program that prints with put function 
+ * 
+ * return: always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
