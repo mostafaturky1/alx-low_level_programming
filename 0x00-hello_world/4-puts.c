13a22f71bdc4f0a23bf7743035ced32f0a259447
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-08-10 15:03:52 -0700

    “commit”

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/**
- *main - entry point
+
+/*
+ * main - Entry point
  * 
- *Description: a c program that prints with put function 
+ * Description: A C program that prints with put function 
  * 
- * return: always 0 (success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
