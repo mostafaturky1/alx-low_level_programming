40ebc3190e8f737f7d401a4c928c56902b46aee4
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-08-17 13:54:00 -0700

    “commit”

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters
- * @c: char to check
+ * _isupper - check if c is upper
+ * @c: input for alphabet
  *
- * Return: 0 or 1
+ * Return: 1 if its uppercase, 0 if not
 */
 
 int _isupper(int c)
