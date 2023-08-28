57d1cda71f87ccc806cb42832f47c920ab18852e
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-08-28 11:23:49 -0700

    “commit”

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,13 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int j = n; 
-	for (int i = 0; i < j; i++)
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
 	{
-		dest[i] = src[i];
-		n--
+		dest[r] = src[r];
+		n--;
 	}
 	return (dest);
 }
