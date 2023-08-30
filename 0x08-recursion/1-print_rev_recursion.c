762eef40dcab4df9c2e2f6204173ebee4c5c3590
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-08-30 15:19:48 -0700

    “commit”

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
+
+#include "main.h"
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s > '\0')
 	{
-		_strlen_recursion(*s + 1);
+		_print_rev_recursion(*s = 1);
 		_putchar(*s);
 	}
 }
