66003e5a861262c5096f300bfea4f2701423b9fc
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-09-01 13:21:25 -0700

    “commit”

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,11 +16,11 @@ int main(int argc, char *argv[])
 	{
 		i = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", i);
+		return (0);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	return (0);
 }
