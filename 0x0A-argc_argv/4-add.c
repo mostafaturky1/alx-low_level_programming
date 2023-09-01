9f07c96144024bffcef4887410fea90f245a443f
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-09-01 13:45:29 -0700

    “commit”

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv)
+{
+	int add = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		add += atoi(argv[argc]);
+	}
+	printf("%d-n", add);
+	return (0);
+}
