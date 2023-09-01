0ce042ecd140daba6a01fb208c38ddbd9051b99e
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-09-01 14:07:13 -0700

    “commit”

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, lc = 0, money = atoi(argv[1]);
+		int c[] = {25, 10, 2, 5, 1};
+		for (i = 0; i < 5; i++)
+		{
+			if (money >= c[i])
+			{
+				lc += c{i};
+				money = money% c[i];
+				if (money % c[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+	printf("%d\n", lc);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
