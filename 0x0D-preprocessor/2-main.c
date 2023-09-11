22f086402672be31247dc56cfbb32c3642c2397d
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-09-11 13:39:55 -0700

    “commit”

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main: print crrent file name
+ * main - prints the name of the file
  *
- * Return: always 0
+ * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	printf("%s\n", __FILE__);
