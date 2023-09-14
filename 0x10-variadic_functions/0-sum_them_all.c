cf97b037eb86ff87befeb3cdc4bc88a57f57916d
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-09-14 13:22:42 -0700

    “commit”

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,16 +13,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list arg;
 	int sum = 0;
-	int i;
 
 	if (n == 0)
 		return (0);
 
 	va_start(arg, n);
-
-	while (i < n)
+	for (int i = 0; i < n; i++)
 		sum += va_arg(arg, int);
-		i++;
 	va_end(arg);
 	return (sum);
 }
