b1ef4d3c960288381f8ed7bfb6812b20ebf680ca
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-09-26 11:28:54 -0700

    “commit”

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,14 @@
 */
 size_t print_listint(const listint_t *h)
 {
-    size_t i = 0;
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        i++;
-    }
-    return (i);
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
