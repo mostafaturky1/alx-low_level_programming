1aa667c6dfd7dd14c118679448a27def0594b8c4
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-09-26 12:54:24 -0700

    “commit”

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -23,7 +23,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		last = *head;
-		while (last->next != NULL)
+		while (last->next)
 			last = last->next;
 		last->next = new_node;
 	}
