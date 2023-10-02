6720dba259df2875a9f5dfe7df37544e8bc03209
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-10-02 16:49:33 -0700

    “commit”

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
