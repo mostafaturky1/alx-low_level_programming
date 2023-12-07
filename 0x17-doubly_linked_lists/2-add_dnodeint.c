b3ba2e849a0ed046a2abc658a41d3aab31151ac7
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2023-12-07 23:46:08 +0200

    “commit”

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *newnode;
 
 	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
 	newnode->n = n;
 	newnode->next = *head;
 	newnode->prev = NULL;
