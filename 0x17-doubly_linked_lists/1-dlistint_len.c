b169aedc7ceb441d769c57875817b6ce2c726c8c
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2023-12-07 23:24:00 +0200

    “commit”

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Counts the number of elements in a doubly linked list.
+ * @h: A pointer to the head of the doubly linked list.
+ *
+ * Return: The number of nodes (elements) in the doubly linked list.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
